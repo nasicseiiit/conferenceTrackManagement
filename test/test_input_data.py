d2964326f8151513660960a7fd4044bc0470181d
nasicseiiit <nasicseiiit@gmail.com>
2020-06-20 22:24:07 +0530

    Added test_input_file, test_input_data and input_data file.

---
+++ test/test_input_data.py
@@ -0,0 +1,47 @@
+
+import unittest
+
+from app.input_processor.input_data import get_file_data, check_file_existance
+
+
+class test_file_data(unittest.TestCase):
+    '''
+    test case with a valid file name
+    '''
+    def test_check_file_existance(self):
+        file_name = "/Users/apple/Documents/GitHub/conferenceTrackManagement/input_data/test_input_file"
+        pointer_to_file = check_file_existance(file_name)
+        self.assertNotEqual(pointer_to_file, False)
+        pointer_to_file.close()
+
+    '''
+        test case with a invalid file
+    '''
+
+    def test_check_file_existance_error(self):
+        file_name = "/Users/apple/Documents/GitHub/conferenceTrackManagement/input_data/ramdom_file"
+        pointer_to_file = check_file_existance(file_name)
+        self.assertEqual(pointer_to_file, False)
+
+    '''
+    test case to read the input_data from the input file
+    '''
+    def test_get_file_data(self):
+        file_name = "/Users/apple/Documents/GitHub/conferenceTrackManagement/input_data/test_input_file"
+        expected_file_data = ["Writing Fast Tests Against Enterprise Rails 60min","Overdoing it in Python 45min"]
+        actual_file_data = get_file_data(file_name)
+        print(actual_file_data)
+        self.assertEqual(expected_file_data, actual_file_data)
+
+    '''
+        test case to read the input_data from the input file if the file does not exist
+    '''
+    def test_get_file_data_empty_list(self):
+        file_name = "/Users/apple/Documents/GitHub/conferenceTrackManagement/input_data/random_file"
+        expected_file_data = []
+        actual_file_data = get_file_data(file_name)
+        print(actual_file_data)
+        self.assertEqual(expected_file_data, actual_file_data)
+
+if __name__ == '__main__':
+    unittest.main()
