f67dfc2005cc61baca75c9abb0a164e62f5ab4b6
nasicseiiit <nasicseiiit@gmail.com>
2020-06-24 22:55:30 +0530

    Created set up file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='conferenceTrackManagement',
+    version='',
+    packages=['test'],
+    url='',
+    license='',
+    author='nasimunni',
+    author_email='nasicseiiit@gmail.com',
+    description=''
+)
