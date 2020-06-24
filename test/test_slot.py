a908069ef628a8214f806e79aa16e1fd8005906e
nasicseiiit <nasicseiiit@gmail.com>
2020-06-24 11:48:18 +0530

    Added test_slot file with the test cases of slot.py

--- test/test_slot.py
+++ test/test_slot.py
@@ -1,40 +1,46 @@
 
 import unittest
 
-from app.input_processor.input_data_validator import mapping_duration_to_lightning_talk, mapping_duration_to_min_talk, \
-     validate_input_data
-
+from app.conference_track_management.slot import fill_slot, event, has_room
+from app.constants import constants
 
 class test_file_data(unittest.TestCase):
     '''
-    test case to map duration time to lightning talk
+    test case to fill the slot with the talk
     '''
-    def test_mapping_duration_to_lightning_talk(self):
-        map_talk_and_time = {}
-        talks_with_lightning = ["Rails for Python Developers lightning"]
-        expected_answer = {'Rails for Python Developers lightning': 5}
-        actual_answer = mapping_duration_to_lightning_talk(map_talk_and_time, talks_with_lightning)
+    def test_fill_slot(self):
+        start_time = constants.TRACK_START_TIME
+        session_duration = constants.MORNING_SLOT_DURATION_MINUTES
+        talks = [('Writing Fast Tests Against Enterprise Rails 60min', 60), ('Rails for Python Developers lightning', 5)]
+        expected_answer = ['09:00 Writing Fast Tests Against Enterprise Rails 60min', '10:00 Rails for Python Developers lightning']
+        actual_answer = fill_slot(start_time, session_duration, talks)
         print(actual_answer)
         self.assertEqual(expected_answer, actual_answer)
 
     '''
-        test case to map duration time to min talk
+        test case to create the event 
         '''
-    def test_mapping_duration_to_min_talk(self):
-        map_talk_and_time = {'Rails for Python Developers lightning': 5}
-        talks_with_min = ["Writing Fast Tests Against Enterprise Rails 60min"]
-        expected_answer = {'Rails for Python Developers lightning': 5, 'Writing Fast Tests Against Enterprise Rails 60min': 60}
-        actual_answer = mapping_duration_to_min_talk(map_talk_and_time, talks_with_min)
+    def test_event(self):
+        start_time = constants.TRACK_START_TIME
+        talk = "Writing Fast Tests Against Enterprise Rails 60min"
+        expected_answer = "09:00 Writing Fast Tests Against Enterprise Rails 60min"
+        actual_answer = event(start_time, talk)
         print(actual_answer)
         self.assertEqual(expected_answer, actual_answer)
 
-    def test_validate_input_data(self):
-        file_data = ["Writing Fast Tests Against Enterprise Rails 60min","Overdoing it in Python lightningjfjdk","Rails for Python Developers lightning"]
-        expected_answer = {'Rails for Python Developers lightning': 5,
-                           'Writing Fast Tests Against Enterprise Rails 60min': 60}
-        actual_answer = validate_input_data(file_data)
-        print(actual_answer)
-        self.assertEqual(expected_answer, actual_answer)
+
+    def test_has_room(self):
+        talk_duration = 60
+        session_duration = 180
+        self.assertTrue(has_room(talk_duration,session_duration))
+
+    '''
+            test case to check has room or not by returning false
+            '''
+    def test_has_room_returns_false(self):
+        talk_duration = 180
+        session_duration = 60
+        self.assertFalse(has_room(talk_duration,session_duration))
 
 if __name__ == '__main__':
     unittest.main()
