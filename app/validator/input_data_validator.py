24da00a92b0a4fa89bdb96270c9ede9468216463
nasicseiiit <nasicseiiit@gmail.com>
2020-06-25 15:41:41 +0530

    Modified the input_data_validator file.
    Moved the below code to top of the file.
    
    SUBTRACT_THREE = -3
    SUBTRACT_ONE = -1

--- app/validator/input_data_validator.py
+++ app/validator/input_data_validator.py
@@ -6,6 +6,9 @@ Else the talk will be discarded
 '''
 from app.constants import constants
 
+SUBTRACT_THREE = -3
+SUBTRACT_ONE = -1
+
 def validate_input_data(file_data):
     talks_with_lightning = lines_ending_with(file_data, constants.LIGHTNING)
     talks_with_min = lines_ending_with(file_data, constants.MINUTES)
@@ -24,12 +27,10 @@ def mapping_duration_to_lightning_talk(map_talk_and_time, talks_with_lightning):
 adding the minute talks
 '''
 def mapping_duration_to_min_talk(map_talk_and_time, talks_with_min):
-    SUBTRACT_THREE = 3
-    SUBTRACT_ONE = 1
     for line in talks_with_min:
         split_line = line.split()
-        minutes_string = split_line[-SUBTRACT_ONE]
-        duration = int(minutes_string[:-SUBTRACT_THREE])
+        minutes_string = split_line[SUBTRACT_ONE]
+        duration = int(minutes_string[:SUBTRACT_THREE])
         map_talk_and_time[line] = duration
     return map_talk_and_time
 
